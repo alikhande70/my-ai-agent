039b16c27b7824a4daac6aab3b2bc6af77a43133
alikhande70 <alikhande70@gmail.com>
2026-06-02 00:57:49 +0330

    chore: add package initialization

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+"""AI Agent Package - Professional Implementation"""
+
+__version__ = "1.0.0"
+__author__ = "alikhande70"
+
+from src.core.agent import AIAgent
+from src.core.config import Config
+
+__all__ = ["AIAgent", "Config"]
